78009d080f05266f071ad22ba231847e45925ed6
lwc <weichien00@gmail.com>
2025-08-19 21:52:26 +0800

    hide component in dashboard

--- src/app/[lang]/(dashboard)/(private)/dashboards/crm/page.tsx
+++ src/app/[lang]/(dashboard)/(private)/dashboards/crm/page.tsx
@@ -42,77 +42,77 @@ import { getUserData } from '@/app/server/actions'
 } */
 
 const DashboardCRM = async () => {
-  // Vars
-  const data = await getUserData()
-  const serverMode = await getServerMode()
+    // Vars
+    const data = await getUserData()
+    const serverMode = await getServerMode()
 
-  return (
-    <Grid container spacing={6}>
-      <Grid size={{ xs: 12, md: 4 }}>
-        <Award />
-      </Grid>
-      <Grid size={{ xs: 12, md: 2, sm: 3 }}>
-        <CardStatVertical
-          stats='155k'
-          title='Total Orders'
-          trendNumber='22%'
-          chipText='Last 4 Month'
-          avatarColor='primary'
-          avatarIcon='ri-shopping-cart-line'
-          avatarSkin='light'
-          chipColor='secondary'
-        />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 3, md: 2 }}>
-        <CardStatVertical
-          stats='$13.4k'
-          title='Total Sales'
-          trendNumber='38%'
-          chipText='Last Six Months'
-          avatarColor='success'
-          avatarIcon='ri-handbag-line'
-          avatarSkin='light'
-          chipColor='secondary'
-        />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 3, md: 2 }}>
-        <StackedBarChart />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 3, md: 2 }}>
-        <DonutChart />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }}>
-        <OrganicSessions />
-      </Grid>
-      <Grid size={{ xs: 12, md: 8 }}>
-        <ProjectTimeline />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-        <WeeklyOverview />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-        <SocialNetworkVisits />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-        <MonthlyBudget />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-        <MeetingSchedule />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-        <ExternalLinks />
-      </Grid>
-      <Grid size={{ xs: 12, sm: 6, md: 4 }}>
-        <PaymentHistory serverMode={serverMode} />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }}>
-        <SalesInCountries />
-      </Grid>
-      <Grid size={{ xs: 12, md: 8 }}>
-        <UserTable tableData={data} />
-      </Grid>
-    </Grid>
-  )
+    return (
+        <Grid container spacing={6}>
+            <Grid size={{ xs: 12, md: 4 }}>
+                <Award />
+            </Grid>
+            {/* <Grid size={{ xs: 12, md: 2, sm: 3 }}>
+                <CardStatVertical
+                    stats='155k'
+                    title='Total Orders'
+                    trendNumber='22%'
+                    chipText='Last 4 Month'
+                    avatarColor='primary'
+                    avatarIcon='ri-shopping-cart-line'
+                    avatarSkin='light'
+                    chipColor='secondary'
+                />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 3, md: 2 }}>
+                <CardStatVertical
+                    stats='$13.4k'
+                    title='Total Sales'
+                    trendNumber='38%'
+                    chipText='Last Six Months'
+                    avatarColor='success'
+                    avatarIcon='ri-handbag-line'
+                    avatarSkin='light'
+                    chipColor='secondary'
+                />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 3, md: 2 }}>
+                <StackedBarChart />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 3, md: 2 }}>
+                <DonutChart />
+            </Grid>
+            <Grid size={{ xs: 12, md: 4 }}>
+                <OrganicSessions />
+            </Grid>
+            <Grid size={{ xs: 12, md: 8 }}>
+                <ProjectTimeline />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                <WeeklyOverview />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                <SocialNetworkVisits />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                <MonthlyBudget />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                <MeetingSchedule />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                <ExternalLinks />
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, md: 4 }}>
+                <PaymentHistory serverMode={serverMode} />
+            </Grid>
+            <Grid size={{ xs: 12, md: 4 }}>
+                <SalesInCountries />
+            </Grid>
+            <Grid size={{ xs: 12, md: 8 }}>
+                <UserTable tableData={data} />
+            </Grid> */}
+        </Grid>
+    )
 }
 
 export default DashboardCRM
